34521caecfdacb0d5852d3d99b440d7b263ffe84
juanjodc <juanjosepl@alumnos.iesgalileo.es>
2023-04-12 19:37:22 +0200

    v1 * Alumno.java constructor * Programa.java vacio

---
+++ Alumno.java
@@ -0,0 +1,11 @@
+class Alumno
+{
+   //atirbutos
+   private int nm;
+   private String nombre;
+   //constructor
+   public Alumno(int nm, String nombre) {
+      this.nm=nm;
+      this.nombre=nombre;
+   }
+}
